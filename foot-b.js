f0f393b446a2a3e30e47b1f3601fae6746762173
rrf <67052844+rrf-27@users.noreply.github.com>
2023-05-30 08:22:57 +0300

    Update foot-b.js

--- foot-b.js
+++ foot-b.js
@@ -1,3 +1,5 @@
+
+
 var ft = document.getElementById("foot");
 var paragraphs = document.getElementsByClassName("art-paragraph");
 var j = 0;
@@ -7,6 +9,10 @@ for(var i = 0; i<paragraphs.length; i++)
 }
 
 if(window.innerHeight > j) {
-    ft.style.position = "absolute";
-    ft.style.top =  (window.innerHeight - ft.clientHeight)+ "px";
+    ft.style.position = "relative";
+    ft.style.marginTop =  (window.innerHeight - j - ft.clientHeight)+ "px";
+}
+var footBtn = document.getElementsByClassName("case-middle")[0];
+footBtn.onclick = () => {
+    window.scrollTo(0, 0);
 }
